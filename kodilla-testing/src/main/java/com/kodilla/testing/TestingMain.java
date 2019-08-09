f77f79b2dc4e0c9f70fc4d5df27123a7c1f14d39
Skorpien <30352529+Skorpien@users.noreply.github.com>
2019-08-09 16:11:07 +0200

    Add class and test Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+public class TestingMain<addResult> {
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(2, 3);
+        int subResult = calculator.subtract(3, 2);
+
+        if (addResult == 5) {
+            System.out.println("Calculator add test - OK");
+        } else {
+            System.out.println("Calculator add test - Error!");
+        }
+
+        if (subResult == 1) {
+            System.out.println("Calculator subtract test - OK");
+        } else {
+            System.out.println("Calculator add test - Error!");
+        }
     }
 }
