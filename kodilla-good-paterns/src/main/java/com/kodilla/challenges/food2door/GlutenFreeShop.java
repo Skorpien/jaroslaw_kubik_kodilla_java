deaa3c5d1eb828f2a3b0a55cdb41881df71f7866
Skorpien <30352529+Skorpien@users.noreply.github.com>
2019-11-02 18:07:09 +0100

    Module 13.4 - fix

--- kodilla-good-paterns/src/main/java/com/kodilla/challenges/food2door/GlutenFreeShop.java
+++ kodilla-good-paterns/src/main/java/com/kodilla/challenges/food2door/GlutenFreeShop.java
@@ -5,7 +5,7 @@ public class GlutenFreeShop implements Producer {
 
     @Override
     public boolean process (Order order) {
-        if (order.getProduct()==null || order.getAmount()==0) {
+        if (order.getProduct().equals(null) || order.getAmount()==0) {
             System.out.println("Out of stock");
             return false;
         }
