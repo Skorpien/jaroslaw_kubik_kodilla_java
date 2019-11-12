9d76893aa214599aae53d7f52bc0be10888c19c1
Skorpien <30352529+Skorpien@users.noreply.github.com>
2019-11-12 21:49:27 +0100

    Module 15.2 - fix

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -20,7 +20,7 @@ public class LibraryTestSuite {
         try {
             cloneLibrary = library.shallowCopy();
         } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
 
         //making a deep copy
@@ -28,24 +28,18 @@ public class LibraryTestSuite {
         try {
             deepClonedLibrary = library.deepCopy();
         } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
 
         //When
         library.getBooks().removeIf((Book b) -> b.getPublicationDate().getYear() < 2003);
 
         //Then
-        System.out.println(library.getName());
         Assert.assertEquals("Library", library.getName());
-        System.out.println(library.getBooks());
         Assert.assertEquals(3, library.getBooks().size());
-        System.out.println(cloneLibrary.getName());
         Assert.assertEquals("Library", cloneLibrary.getName());
-        System.out.println(cloneLibrary.getBooks());
         Assert.assertEquals(3, cloneLibrary.getBooks().size());
-        System.out.println(deepClonedLibrary.getName());
         Assert.assertEquals("Library", deepClonedLibrary.getName());
-        System.out.println(deepClonedLibrary.getBooks());
         Assert.assertEquals(5, deepClonedLibrary.getBooks().size());
     }
 }
