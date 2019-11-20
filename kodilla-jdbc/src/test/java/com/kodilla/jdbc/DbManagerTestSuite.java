970a0a0983ba13ae149bf60650acb90b60221e06
Skorpien <30352529+Skorpien@users.noreply.github.com>
2019-11-20 11:02:52 +0100

    Module 16.6

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,69 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbManagerTestSuite {
+    @Test
+    public void testConnect() throws SQLException {
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    public void testSelectUsers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery = "SELECT U.LASTNAME, U.FIRSTNAME, COUNT(*) AS POSTS_NUMBER " +
+                "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID " +
+                "GROUP BY U.ID";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            if (rs.getInt("POSTS_NUMBER")>1) {
+                System.out.println(rs.getString("LASTNAME") + ", " +
+                        rs.getString("FIRSTNAME") + ", " +
+                        rs.getInt("POSTS_NUMBER"));
+                counter++;
+            }
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(1, counter);
+    }
+}
