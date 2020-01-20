77190dc988790cf47ff70122e93ac375ccdba894
Skorpien <30352529+Skorpien@users.noreply.github.com>
2020-01-21 00:50:23 +0100

    practise

---
+++ kodilla-paterns2/src/main/java/com/kodilla/patterns2/KodillaPaterns2Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.patterns2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaPaterns2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaPaterns2Application.class, args);
+    }
+
+}
